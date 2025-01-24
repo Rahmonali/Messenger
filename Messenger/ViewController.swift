825b55013e69a0f6d99af36e62326036a6d077e1
Rahmonali <rahmonali1995@gmail.com>
2025-01-24 10:55:28 +0500

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Rahmonali on 24/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
